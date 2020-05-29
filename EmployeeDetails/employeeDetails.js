38c59591ea98e6cc920089f273a9d618cde1a1ee
Kiran607 <ickirankiru@gmail.com>
2020-05-29 13:36:38 +0530

    [Kiran]Refactored add image to load in browser

--- EmployeeDetails/employeeDetails.js
+++ EmployeeDetails/employeeDetails.js
@@ -1,18 +1,10 @@
-$.ajax({
-    url: 'employeeData.json',
-    dataType: 'json',
-    success: function (data) {
-        for (let i = 0; i < data.length; i++) {
-            let row = $('<tr><td>' + data[i].firstName + '</td><td>'
-                + data[i].lastName + '</td><td>'
-                + data[i].email + '</td><td>'
-                +data[i].edit+'</td><td>'
-                +data[i].delete+'</td></tr>');
-            $('.table').append(row);
-        }
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-        console.log('Error:' + textStatus + '-' + errorThrown);
-    }
-
+$.getJSON('employeeData.json', function (data) {
+    $.each(data.Employee, function (i, data1) {
+        let tblRow = "<tr><td>" + data1.firstName + "</td>"
+            + "<td>" + data1.lastName + "</td>"
+            + "<td>" + data1.email + "</td>"
+            + "<td>" + "<img src=" + data1.edit +"></td>"
+            + "<td>" + "<img src=" + data1.delete + "></td></tr>"
+        $(tblRow).appendTo(".table");
+    });
 });
